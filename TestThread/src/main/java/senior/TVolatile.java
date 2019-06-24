9d0e27e29e45c328e47dce61ead8f0cdb465f64d
FatePsalm <32047409+FatePsalm@users.noreply.github.com>
2019-06-25 00:08:05 +0800

    1

--- TestThread/src/main/java/senior/TVolatile.java
+++ TestThread/src/main/java/senior/TVolatile.java
@@ -4,12 +4,21 @@ package senior;
  * @author 作者 Name:CaoGang
  * @version 创建时间：2018年4月3日 下午5:32:08 类说明 volatile 修饰变量的内存可见性
  */
+/*
+ * 一、volatile 关键字：当多个线程进行操作共享数据时，可以保证内存中的数据可见。
+ * 					  相较于 synchronized 是一种较为轻量级的同步策略。
+ *
+ * 注意：
+ * 1. volatile 不具备“互斥性”
+ * 2. volatile 不能保证变量的“原子性”
+ */
 public class TVolatile {
 	public static void main(String[] args) throws InterruptedException {
 		ThreadDemo threadDemo = new ThreadDemo();
 		new Thread(threadDemo).start();
 		while (true) {
-			if (threadDemo.isFlag()) {
+			boolean flag = threadDemo.isFlag();
+			if (flag) {
 				System.out.println("******************");
 				break;
 			}
